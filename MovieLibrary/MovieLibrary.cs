a721b517ad35a0c084c25be18dc412df950c8d48
manuxx <manusxyz@gmail.com>
2022-05-12 17:46:03 +0200

    zaimplementowane, wyczyszczone

--- MovieLibrary/MovieLibrary.cs
+++ MovieLibrary/MovieLibrary.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Buffers;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace TrainingPrep.collections
@@ -16,7 +15,7 @@ namespace TrainingPrep.collections
 
         public IEnumerable<Movie> all_movies()
         {
-            return new ReadOnlySet(movies);
+            return new ReadOnlySet<Movie>(movies);
         }
 
         public void add(Movie movie)
@@ -27,26 +26,6 @@ namespace TrainingPrep.collections
                     return;
             }
             movies.Add(movie);
-        }
-    }
-
-    public class ReadOnlySet : IEnumerable<Movie>
-    {
-        private readonly IEnumerable<Movie> _movies;
-
-        public ReadOnlySet(IEnumerable<Movie> movies)
-        {
-            _movies = movies;
-        }
-
-        public IEnumerator<Movie> GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
         }
     }
 }
