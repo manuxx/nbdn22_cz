65355d32b87e27a452c9cd30c9015842b53ecafd
manuxx <manusxyz@gmail.com>
2022-05-26 17:19:38 +0200

    zadanie not

--- MovieLibrarySpec/MovieLibrarySpecs.cs
+++ MovieLibrarySpec/MovieLibrarySpecs.cs
@@ -221,10 +221,12 @@ namespace TrainingPrep.specs
                 results.ShouldContainOnly(a_bugs_life, pirates_of_the_carribean, cars);
             };
 
-           
-            It should_be_able_to_find_all_movies_not_published_by_pixar = () =>
+
+            private It should_be_able_to_find_all_movies_not_published_by_pixar = () =>
             {
-                var results = subject.all_movies_not_published_by_pixar();
+                var criteria = Where<Movie>.HasAn(m => m.production_studio).Not().EqualTo(ProductionStudio.Pixar);
+                
+                var results = subject.all_movies().ThatSatisfy(criteria);
 
                 results.ShouldNotContain(cars, a_bugs_life);
             };
