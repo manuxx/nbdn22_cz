3e024e420d862823c349c894d1ef992da74f9941
manuxx <manusxyz@gmail.com>
2022-05-26 16:44:38 +0200

    to samo troche lepiej zapisane

--- MovieLibrary/CriteriaBuilder.cs
+++ MovieLibrary/CriteriaBuilder.cs
@@ -15,7 +15,8 @@ namespace TrainingPrep.specs.MovieLibrarySpecs
         {
             return new AnonymousCriteria<TItem>(movie => _selector(movie).Equals(value));
         }
-        public Criteria<TItem> GreaterThan(IComparable<TProperty> value)
+        public Criteria<TItem> GreaterThan<IComparableProperty>(IComparableProperty value) 
+                                                where IComparableProperty :  IComparable<TProperty>
         {
             return new AnonymousCriteria<TItem>(movie => value.CompareTo(_selector(movie)) < 0);
         }
